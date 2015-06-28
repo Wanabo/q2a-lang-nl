7dfaad9be9867cac22f330329180909a717bdb2d
Wanabo <post2wim@gmail.com>
2015-06-28 16:07:02 +0200

    Create qa-lang-users.php

---
+++ nl/qa-lang-users.php
@@ -0,0 +1,142 @@
+<?php
+/*
+	Question2Answer 1.7.0 (c) 2015, Gideon Greenspan
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-users.php
+	Version: 1.7.0
+	Date: 2015-06-28 06:58:57 GMT
+	Description: Language phrases for email notifications
+	Language file updated from 1.6.x to 1.7.0 by http://www.klokkenluider.org
+	Keep file as UTF-8 by http://www.klokkenluider.org àáâãäåæçèéêëìíîïñòóôõöùúûüý
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+return array(
+	'about' => 'Over',
+	'add_user_x_favorites' => "Voeg gebruiker ^ toe aan mijn favorieten",
+	'approve_required' => "Wacht alsjeblieft op de goedkeuring van je account of ^1voeg meer informatie toe^2.",
+	'approve_title' => "Gebruiker in afwachting van goedkeuring",
+	'approve_user_button' => "Gebruiker Goedkeuren",
+	'approved_user' => "Goedgekeurde gebruiker",
+	'avatar_default' => 'Standaard',
+	'avatar_gravatar' => 'Toon mijn ^1Gravatar^2',
+	'avatar_label' => 'Avatar:',
+	'avatar_none' => 'Geen',
+	'block_user_button' => 'Blokkeer gebruiker',
+	'blocked_users' => 'Geblokkeerde gebruikers',
+	'category_level_add' => " - ^1add categorie-specifieke privileges^2",
+	'category_level_in' => "voor de hiernavolgende categorie:",
+	'category_level_label' => "Upgraded to:",
+	'category_level_none' => "No upgrade",
+	'change_email_link' => " - ^1verander email^2",
+	'change_password' => 'Wachtwoord wijzigen',
+	'confirm_complete' => 'Bedankt - Je e-mailadres is nu bevestigd!',
+	'confirm_emailed' => 'Een bevestiginsmail is onderweg. Klik op de link daarin om je e-mailadres te bevestigen.',
+	'confirm_required' => "Om uw registratie te voltooien moet je op de link klikken die naar je gemaild is, of ^1een nieuwe aanvragen^2.",
+	'confirm_title' => 'Emailadres bevestiging',
+	'confirm_wrong_log_in' => 'Code niet juist - aub. ^1inloggen^2 om een nieuwe link toe te laten sturen',
+	'confirm_wrong_resend' => 'Code niet juist - klik hieronder om een nieuwe link toe te laten sturen',
+	'delete_user_button' => "Verwijder Gebruiker",
+	'edit_profile' => "Bewerk mijn Profiel",
+	'edit_user_button' => 'Bewerk deze gebruiker',
+	'email_confirmed' => 'Bevestigd',
+	'email_exists' => 'Emailadres al in gebruik bij ander account',
+	'email_handle_label' => 'Emailadres of gebruikersnaam:',
+	'email_invalid' => 'Emailadres ongeldig. Controleer en probeer opnieuw',
+	'email_label' => 'Emailadres:',
+	'email_not_confirmed' => 'Nog niet bevestigd',
+	'email_please_confirm' => 'Aub. ^5bevestigen^6',
+	'email_required' => 'Emailadres vereist (niet publiekelijk)',
+	'forgot_link' => 'Wachtwoord vergeten',
+	'full_name' => 'Volledige naam',
+	'handle_empty' => 'Gebruikersnaam mag niet leeg zijn',
+	'handle_exists' => 'Username in gebruik - probeer een andere',
+	'handle_has_bad' => 'Gebruikersnaam mag het volgende niet bevatten: ^',
+	'handle_label' => 'Gebruikersnaam:',
+	'hide_all_user_button' => 'Verberg alle berichten van deze gebruiker',
+	'last_login_label' => 'Laaste inlog:',
+	'last_write_label' => 'Laatse schrijfactie:',
+	'level_admin' => 'Administrator',
+	'level_editor' => 'Bewerker',
+	'level_expert' => 'Expert',
+	'level_for_category' => "^1 voor ^2",
+	'level_in_general' => "in het algemeen",
+	'level_moderator' => 'Moderator',
+	'level_super' => 'Super Administrator',
+	'location' => 'Locatie',
+	'log_in_to_access' => 'Je kunt nu ^1inloggen^2 om toegang te krijgen tot je account.',
+	'login_button' => 'Log in',
+	'login_limit' => 'Teveel inlogpogingen. Probeer nogmaals over een uur.',
+	'login_title' => 'Inloggen',
+	'mass_mailings' => "Site mailing:",
+	'mass_mailings_explanation' => "Aanmelden voor emails bestemd voor alle gebruikers",
+	'member_for' => 'Lid sinds:',
+	'member_type' => 'Type:',
+	'new_password_1' => 'Nieuw wachtwoord:',
+	'new_password_2' => 'Wachtwoord opnieuw:',
+	'no_blocked_users' => 'Geen geblokkeerde gebruikers gevonden',
+	'no_permission' => 'Je hebt geen rechten om deze actie uit te voeren',
+	'old_password' => 'Huidige wachtwoord:',
+	'only_shown_admins' => '(alleen zichtbaar voor beheerders)',
+	'only_shown_editors' => "(alleen zichtbaar voor bewerkers en hoger)",
+	'only_shown_experts' => "(alleen zichtbaar voor experts en hoger)",
+	'only_shown_moderators' => '(alleen zichtbaar voor moderators)',
+	'password_changed' => 'Wachtwoord gewijzigd',
+	'password_label' => 'Wachtwoord:',
+	'password_min' => 'Wachtwoord moet uit ten minste ^ tekens bestaan',
+	'password_mismatch' => 'Wachtwoorden komen niet overeen',
+	'password_none' => 'Geen. Om direct in te loggen, stel hieronder een wachtwoord in:',
+	'password_sent' => 'Je nieuwe wachtwoord is per e-mail verzonden',
+	'password_to_set' => 'Stel aub. in op je accountpagina',
+	'password_wrong' => 'Wachtwoord onjuist',
+	'private_messages' => 'Privéberichten:',
+	'private_messages_explanation' => 'Sta gebruikers toe mij te mailen (zonder mijn adres te zien)',
+	'profile_saved' => 'Profiel opgeslagen',
+	'register_button' => 'Registeren',
+	'register_limit' => "Te veel registraties - probeer het over een uur opnieuw",
+	'register_suspended' => 'Registratie van nieuwe gebruikers is tijdelijk uitgeschakeld. Probeer het later nogmaals.',
+	'register_title' => 'Registreer als nieuwe gebruiker',
+	'registered_label' => "Geregistreerd:",
+	'registered_user' => 'Geregistreerde gebruiker',
+	'remember' => "Onthouden",
+	'remember_label' => 'Herinner mij op deze computer',
+	'remove_avatar' => "Verwijder avatar:",
+	'reset_code_another' => 'stuur nog een',
+	'reset_code_emailed' => 'Je reset-code is per e-mail verzonden',
+	'reset_code_label' => 'Code:',
+	'reset_code_wrong' => 'Code onjuist',
+	'reset_title' => 'Vergeten wachtwoord herstellen',
+	'save_profile' => 'Profiel opslaan',
+	'save_user' => 'Gebruiker opslaan',
+	'send_confirm_button' => 'Stuur bevestigingslink',
+	'send_password_button' => 'Stuur nieuw wachtwoord',
+	'send_reset_button' => 'Stuur wachtwoordherstel e-mail',
+	'send_reset_note' => 'Een bericht met instructies wordt per e-mail verzonden.',
+	'special_users' => 'Speciale gebruikers',
+	'terms_not_accepted' => "U moet hier een vinkje zetten!",
+	'unblock_user_button' => 'Deblokkeer gebruiker',
+	'unsubscribe' => "Afmelden:",
+	'unsubscribe_complete' => "Je bent afgemeld voor site mailings door ^0. Opnieuw aanmelden kan altijd op uw ^1account^2 pagina.",
+	'unsubscribe_title' => "Afmelden",
+	'unsubscribe_wrong_log_in' => "Code incorrect - ^1log alsjeblieft in^2 om uit te schrijven",
+	'user_blocked' => '(geblokkeerd)',
+	'user_not_found' => 'Gebruiker niet gevonden',
+	'wall_posts' => "Prikbord berichten:",
+	'wall_posts_explanation' => "Gebruikers toestaan op je prikbord berichten te plaatsen (je ontvangt ook een e-mail)",
+	'website' => 'Website',
+	'x_ago_from_y' => '^1 geleden van ^2',
+);
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
