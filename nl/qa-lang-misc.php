10a8fcc393ffbdfaa854148dd450986d88f8571f
Wanabo <post2wim@gmail.com>
2015-06-28 16:04:39 +0200

    Create qa-lang-misc.php

---
+++ nl/qa-lang-misc.php
@@ -0,0 +1,124 @@
+<?php
+/*
+	Question2Answer 1.7.0 (c) 2015, Gideon Greenspan
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-misc.php
+	Version: 1.7.0
+	Date: 2015-06-28 06:58:57 GMT
+	Description: Language phrases for email notifications
+	Language file updated from 1.6.x to 1.7.0 by http://www.klokkenluider.org
+	Keep file as UTF-8 by http://www.klokkenluider.org àáâãäåæçèéêëìíîïñòóôõöùúûüý
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+return array(
+	'block_ip_button' => 'Blokkeer IP adres',
+	'browse_categories' => 'Blader in categorieën',
+	'captcha_approve_fix' => "Deze verificatie verdwijnt wanneer je account is goedgekeurd.",
+	'captcha_confirm_fix' => 'Om deze controle in de toekomst te vermijden, ^5bevestig je e-mailadres^6.',
+	'captcha_error' => 'Voltooi aub de anti-spam verificatie',
+	'captcha_label' => 'Anti-spam verificatie:',
+	'captcha_login_fix' => 'Om deze controle in de toekomst te vermijden, ^1log in^2 of ^3registreer^4.',
+	'feed_a_edited_prefix' => 'Antwoord bewerkt: ',
+	'feed_a_prefix' => 'Beantwoord: ',
+	'feed_a_reshown_prefix' => "Opnieuw getoonde antwoord: ",
+	'feed_a_selected_prefix' => "Geselecteerd antwoord: ",
+	'feed_c_edited_prefix' => 'Opmerking bewerkt: ',
+	'feed_c_moved_prefix' => "Opmerking verplaatst: ",
+	'feed_c_prefix' => 'Van opmerking voorzien: ',
+	'feed_c_reshown_prefix' => "Opnieuw getoonde commentaar: ",
+	'feed_closed_prefix' => "Gesloten: ",
+	'feed_edited_prefix' => 'Bewerkt: ',
+	'feed_hidden_prefix' => "Verborgen: ",
+	'feed_not_found' => 'Feed niet gevonden',
+	'feed_recategorized_prefix' => "Gehercategoriseerd: ",
+	'feed_reopened_prefix' => "Heropend: ",
+	'feed_reshown_prefix' => "Opnieuw getoond: ",
+	'feed_retagged_prefix' => "Hertagd: ",
+	'feedback_email' => 'Je email: (optioneel)',
+	'feedback_empty' => 'Gebruik dit veld om opmerkingen of suggesties te doen',
+	'feedback_message' => 'Je opmerkingen of suggesties voor ^:',
+	'feedback_name' => 'Je naam: (optioneel)',
+	'feedback_sent' => 'Onderstaand bericht is verzonden. Bedankt.',
+	'feedback_title' => 'Stuur feedback',
+	'form_security_again' => "Klik alstublieft opnieuw om te bevestigen.",
+	'form_security_reload' => "Herlaad alstublieft de pagina en probeer het opnieuw.",
+	'hide_all_ip_button' => 'Alle berichten van dit IP verbergen',
+	'host_name' => 'Hostnaam:',
+	'inbox' => "Inbox",
+	'matches_blocked_ips' => 'Komt overeen met geblokkeerde ip-adressen:',
+	'message_empty' => 'Voer je bericht in om naar deze gebruiker te sturen:',
+	'message_explanation' => 'Dit wordt verstuurd als een bericht van ^. Je e-mailadres wordt niet kenbaar gemaakt, tenzij je dit zelf vermeld in het bericht.',
+	'message_for_x' => 'Je bericht aan ^:',
+	'message_limit' => 'Teveel privéberichten verstuurd - probeer opnieuw over een uur',
+	'message_must_login' => 'Aub. ^1inloggen^2 of ^3registreren^4 om privéberichten te versturen.',
+	'message_recent_history' => "Recente correspondentie met ^",
+	'message_sent' => 'Je privébericht is verzonden',
+	'more_favorite_qs' => "Meer favoriete vragen...",
+	'more_favorite_tags' => "Meer favoriete tags...",
+	'more_favorite_users' => "Meer favoriete gebruikers...",
+	'my_favorites_title' => "Mijn favorieten",
+	'nav_all_my_updates' => "Al mijn updates",
+	'nav_my_content' => "Mijn bijdragen",
+	'nav_my_details' => "Mijn details",
+	'nav_my_favorites' => "Mijn favorieten",
+	'nav_user_activity' => "Recente activiteit",
+	'nav_user_as' => "Alle antwoorden",
+	'nav_user_pms' => "Privé berichten",
+	'nav_user_qs' => "Alle vragen",
+	'nav_user_wall' => "Prikbord",
+	'no_activity_from_x' => 'Geen activiteit van ^',
+	'no_favorite_categories' => "Geen favoriete categorieën",
+	'no_favorite_qs' => "Geen favoriete vragen",
+	'no_favorite_tags' => "Geen favoriete tags",
+	'no_favorite_users' => "Geen favoriete gebruikers",
+	'no_recent_updates' => "Geen recente updates",
+	'no_updates_content' => "Geen recente updates bij mijn bijdrages",
+	'no_updates_favorites' => "Geen updates bij mijn favorieten",
+	'outbox' => "Verstuurde berichten",
+	'pm_inbox_title' => "Privé berichten ontvangen",
+	'pm_outbox_title' => "Privé berichten verstuurd",
+	'private_message_title' => 'Stuur een privébericht',
+	'recent_activity_from_x' => 'Recent activiteit van ^',
+	'recent_updates_content' => "Recente updates bij mijn bijdrages",
+	'recent_updates_favorites' => "Recente updates bij mijn favorieten",
+	'recent_updates_title' => "Recente updates voor mij",
+	'site_in_maintenance' => 'Deze site is momenteel niet beschikbaar vanwege onderhoud. Probeer het later nogmaals.',
+	'suggest_favorites_add' => "Om een vraag toe te voegen aan uw favorieten, klik op de ^ top van die pagina.",
+	'suggest_update_favorites' => "Voor meer updates,  voeg items toe ^1aan uw favorieten^2.",
+	'unblock_ip_button' => 'Deblokkeer IP adres',
+	'your_a_commented' => "opmerking op je antwoord",
+	'your_a_edited' => "je antwoord bewerkt",
+	'your_a_hidden' => "je antwoord verborgen",
+	'your_a_questioned' => "vroeg aan je antwoord",
+	'your_a_reshown' => "je antwoord zichtbaar gemaakt",
+	'your_a_selected' => "selecteerde je antwoord",
+	'your_c_edited' => "je opmerking bewerkt",
+	'your_c_followed' => "je opmerking gevolgd",
+	'your_c_hidden' => "je opmerking verborgen",
+	'your_c_moved' => "je opmerking verplaatst",
+	'your_c_reshown' => "je opmerking zichtbaar gemaakt",
+	'your_q_answered' => "je vraag beantwoord",
+	'your_q_closed' => "je vraag gesloten",
+	'your_q_commented' => "opmerking op je vraag",
+	'your_q_edited' => "je vraag bewerkt",
+	'your_q_hidden' => "je vraag verborgen",
+	'your_q_recategorized' => "je vraag verplaatst",
+	'your_q_reopened' => "je vraag heropend",
+	'your_q_reshown' => "je vraag zichtbaar gemaakt",
+	'your_q_retagged' => "je vraag opnieuw getagd",
+);
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
