483938cf7a157cbeff58c4c2fe9de668c9498b71
Wanabo <post2wim@gmail.com>
2015-06-28 16:05:28 +0200

    Create qa-lang-profile.php

---
+++ nl/qa-lang-profile.php
@@ -0,0 +1,92 @@
+<?php
+/*
+	Question2Answer 1.7.0 (c) 2015, Gideon Greenspan
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-profile.php
+	Version: 1.7.0
+	Date: 2015-06-28 06:58:57 GMT
+	Description: Language phrases for email notifications
+	Language file updated from 1.6.x to 1.7.0 by http://www.klokkenluider.org
+	Keep file as UTF-8 by http://www.klokkenluider.org àáâãäåæçèéêëìíîïñòóôõöùúûüý
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+return array(
+	'1_chosen_as_best' => ' (1 gekozen als beste)',
+	'1_down_vote' => '1 stem omlaag',
+	'1_up_vote' => '1 stem omhoog',
+	'1_with_best_chosen' => ' (1 met het beste antwoord gekozen)',
+	'activity_by_x' => 'Activiteit door ^',
+	'answers' => 'Antwoorden:',
+	'answers_by_x' => "Antwoorden van ^",
+	'bonus_points' => "Bonus punten:",
+	'comments' => 'Opmerkingen:',
+	'delete_pm_popup' => "Verwijder dit privé bericht",
+	'delete_wall_post_popup' => "Verwijder deze prikbordmelding",
+	'extra_privileges' => 'Extra privileges:',
+	'gave_out' => 'Uitgaven:',
+	'my_account_title' => 'Mijn account gegevens',
+	'no_answers_by_x' => "Geen antwoorden van ^",
+	'no_posts_by_x' => 'Geen berichten van ^',
+	'no_questions_by_x' => "Geen vragen van ^",
+	'permit_anon_view_ips' => 'IPs van anonieme posts bekijken',
+	'permit_close_q' => "Het sluiten van een vragen",
+	'permit_delete_hidden' => 'Verborgen posts verwijderen',
+	'permit_edit_a' => 'Elk antwoord bewerken',
+	'permit_edit_c' => 'Elke opmerking bewerken',
+	'permit_edit_q' => 'Elke vraag bewerken',
+	'permit_edit_silent' => "Berichten stil bewerken",
+	'permit_flag' => 'Posts markeren',
+	'permit_hide_show' => 'Elke post tonen of verbergen',
+	'permit_moderate' => "Vragen toestaan of weigeren",
+	'permit_post_a' => 'Vragen beantwoorden',
+	'permit_post_c' => 'Opmerkingen toevoegen',
+	'permit_post_q' => 'Vragen stellen',
+	'permit_post_wall' => "Berichten plaatsen op prikborden gebruikers",
+	'permit_recat' => "Opnieuw categoriseren van vragen",
+	'permit_retag' => "Opnieuw taggen van vragen",
+	'permit_select_a' => 'Antwoord op elke vraag selecteren',
+	'permit_view_q_page' => 'Pagina\'s met vragen bekijken',
+	'permit_view_voters_flaggers' => "Bekijken wie stemde op berichten of deze markeerde",
+	'permit_vote_a' => 'Op antwoorden stemmen',
+	'permit_vote_down' => "Vragen naar beneden stemmen",
+	'permit_vote_q' => 'Op vragen stemmen',
+	'post_wall_blocked' => "Deze gebruiker staat geen nieuwe berichten op zijn prikbord toe",
+	'post_wall_button' => "Plaats bericht",
+	'post_wall_empty' => "Typ alsjeblieft iets om op dit prikbord te plaatsen",
+	'post_wall_limit' => "Je kan geen prikbordberichten meer plaatsen dit uur",
+	'post_wall_must_be_approved' => "Je account moet goedgekeurd zijn om op dit prikbord een bericht te plaatsen.",
+	'post_wall_must_confirm' => "^5Bevestig alsjeblieft je e-mailadres^6 om een bericht op dit prikbord te plaatsen.",
+	'post_wall_must_login' => "^1Log alsjeblieft in^2 of ^3registreer^4 om een bericht op dit prikbord te plaatsen.",
+	'questions' => 'Vragen:',
+	'questions_by_x' => "Vragen van ^",
+	'ranked_x' => ' (rang #^)',
+	'received' => 'Ontvangen:',
+	'recent_activity_by_x' => 'Recente activiteit door ^',
+	'score' => 'Score:',
+	'send_private_message' => ' - ^1stuur privébericht^2',
+	'set_bonus_button' => "Update bonus",
+	'title' => 'Titel:',
+	'user_x' => 'Gebruiker ^',
+	'voted_on' => 'Gestemd op:',
+	'wall_for_x' => "Prikbord van ^",
+	'wall_view_more' => "Bekijk meer prikbord meldingen...",
+	'x_chosen_as_best' => ' (^ gekozen als beste)',
+	'x_down_votes' => '^ stemmen omlaag',
+	'x_up_votes' => '^ stemmen omhoog',
+	'x_with_best_chosen' => ' (^ met beste antwoord gekozen)',
+);
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
